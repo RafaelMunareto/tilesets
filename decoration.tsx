92339b9ace4c8d52a9015197e5cfa53aa0610ed0
Munareto <rafael.munareto@icloud.com>
2022-07-31 09:46:19 -0300

    first commit

---
+++ decoration.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="decoration" tilewidth="16" tileheight="16" tilecount="121" columns="11">
+ <image source="forest_decoration.png" width="176" height="176"/>
+</tileset>
---
+++ tileset_map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="tileset_map" tilewidth="16" tileheight="16" tilecount="540" columns="15">
+ <image source="../../../Downloads/forest.png" width="240" height="576"/>
+</tileset>
